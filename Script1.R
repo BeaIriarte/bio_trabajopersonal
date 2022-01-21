74b2f340d1f410e1b619c9ef233b267447a0bb71
BeaIriarte <biriarte.1@alumni.unav.es>
2022-01-21 10:39:48 +0100

    cambios hechos al final

--- Script1.R
+++ Script1.R
@@ -4,3 +4,32 @@ tir <- MLDataR::thyroid_disease
 
 write.csv(tir, file = "dataset_tiroides.csv")
 
+sessionInfo()
+
+# order: es como un sort de un vector
+
+head(tir)
+
+sort(tir$patient_age)
+
+tir2 <- tir[order(tir$patient_age),]  ## dataset ordenado
+
+## para no tener el NA. Se seleccionan los que tienen completos los datos
+
+tir_completo <- tir[complete.cases(tir),]
+
+# aggregate: agrupar por grupos y hacer un calculo estadistico
+
+# aggregate(valores a calcular (edad), grupos (tir o no), estadístico (media))
+
+aggregate(tir_completo$patient_age, list(tir_completo$ThryroidClass), mean )
+
+
+aggregate(tir_completo$patient_age, list(tir_completo$ThryroidClass, tir_completo$patient_gender), mean )
+
+edad_tir <- aggregate(tir_completo$patient_age, list(tir_completo$ThryroidClass), mean)
+names(edad_tir) <- c("Tiroides", "Media") ## cambiar los nombres de las columnas.
+
+names(edad_tir)
+
+edad_tir
